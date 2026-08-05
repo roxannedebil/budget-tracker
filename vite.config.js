d507c81ca8992b527559857df7996853e87d4351
roxannedebil <roxannedebil14@gmail.com>
2026-08-05 19:03:05 +0800

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/budget-tracker/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
