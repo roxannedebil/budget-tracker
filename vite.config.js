79e25aba82d57086baa7c1bb82a49a66b4cb3da2
roxannedebil <roxannedebil14@gmail.com>
2026-09-10 19:08:27 +0800

    Add GitHub Actions deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/budget-tracker/',
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    visualizer({ open: true })
   ],
 })
